ab962889960a7f921f40a482ac53f702d4a32d07
Joshua Parado <jpa3ado@gmail.com>
2021-03-22 02:51:51 -0400

    done sprint

--- index.js
+++ index.js
@@ -28,12 +28,31 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
+function summation(n) {
+  var result = 0;
+     
+  // One by one compute sum of digits
+  // in every number from 1 to n
+  for (let x = 1; x <= n; x++)
+      result += sumOfDigits(x);
+
+  return result;
 
   }
- 
-
+   function sumOfDigits(x)
+    {
+        var sum = 0;
+         
+        while (x != 0)
+        {
+            sum += x % 10;
+            x = x / 10;
+        }
+         
+        return parseInt(sum);
+        
+    }
+console.log(summation(5));
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
 
@@ -56,9 +75,29 @@ const zooAnimals = [
   displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+  function animalNames(displayNames){
+    var finalobj=[];
+    var element={};
+   for(let i = 0; i < displayNames.length; i++) {
+      
+    
+    
+        element={};
+        element.animal_name=displayNames[i].animal_name;
+        element.scientific_name=displayNames[i].scientific_name;
+        
+        
+       
+        finalobj.push(element);
+    
+   }
+
+  return finalobj;
+}
+console.log(animalNames(zooAnimals)); 
+  
+
+   
   
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
@@ -67,19 +106,43 @@ const zooAnimals = [
   For example: ['jackal, asiatic', .....]
   */
 
-  function lowerCaseNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowerCaseNames(displayNames){
+    var finalobj=[];
+    var element={};
+   for(let i = 0; i < displayNames.length; i++) {
+      
+      
+    element={};
+        element.animal_name=displayNames[i].animal_name;
+        
+        
+        
+       
+        finalobj.push(element.animal_name.toLowerCase());
   }
-  
+
+  return finalobj;
+}
+  console.log(lowerCaseNames(zooAnimals));
   
   /* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
   The zoo is concerned about animals with a lower population count. 
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+  function lowPopulationAnimals(lowPopulation){
+    let animalZoo = lowPopulation.filter(function (e) {
+      return e.population < 5;
+  });
+
+  return animalZoo;
+        
+}
+
+console.log(lowPopulationAnimals(zooAnimals))
+
+
+
   
 
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
@@ -88,10 +151,13 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
+  function USApop(displayNames){
+    let reduced = displayNames
+  .map(item => item.population)
+  .reduce((prev, next) => prev + next);
+  return reduced;
   }
-  
+   console.log(USApop(zooAnimals));
   
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
   /* 🦁🦁🦁 Step 1: Create a higher-order function 🦁🦁🦁
